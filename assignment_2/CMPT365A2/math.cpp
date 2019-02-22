86381181fd691624efe32a32f0541ab89404d524
syonfox <contact@kierlindsay.ca>
2019-02-22 13:34:55 -0800

    added croma subsampling function

--- assignment_2/CMPT365A2/math.cpp
+++ assignment_2/CMPT365A2/math.cpp
@@ -1,5 +1,6 @@
 #include "math.h"
 
+
 static cv::Matx33d dWeights_rgb2yuv(
             0.299,      0.587,      0.114,
             -0.14713,   -0.28886,   0.436,
@@ -13,7 +14,6 @@ static cv::Matx33d dWeights_yuv2rgb(
         );
 
 
-
 cv::Vec3b rgb2yuv(cv::Vec3b pixel){
 
     cv::Vec3b yuv;
@@ -25,7 +25,7 @@ cv::Vec3b rgb2yuv(cv::Vec3b pixel){
     return yuv;
 }
 
-cv::Mat rgb2yuv(cv::Mat img){
+cv::Mat rgb2yuv(cv::Mat &img){
     cv::Mat out;
     out.create(img.size(), CV_8U);
 
@@ -49,13 +49,56 @@ cv::Vec3b yuv2rgb(cv::Vec3b pixel){
     return rgb;
 }
 
-cv::Mat yuv2rgb(cv::Mat img){
+cv::Mat yuv2rgb(cv::Mat &img){
     cv::Mat out;
     out.create(img.size(), CV_8U);
 
     for(int i=0; i<img.rows; i++){
         for (int j=0; j<img.cols; j++) {
             out.at<cv::Vec3b>(i,j) = yuv2rgb(img.at<cv::Vec3b>(i,j));
+        }
+    }
+    return out;
+}
+
+
+cv::Mat chroma_subsample(cv::Mat &img, int a, int b, int c) {
+    cv::Mat out;
+    out.create(img.size(), CV_8U);
+
+    int even_smpl_w = a/b;
+    int odd_smpl_w = 0;
+    if(c != 0){
+      odd_smpl_w = a/c;
+    }
+
+    for(int i=0; i<img.rows; i++){
+        if(i%2 == 0) { //even lines
+            for (int j=0; j<img.cols; j+=even_smpl_w) {
+                for(int k = 1; k<even_smpl_w; k++){
+                    //make smpl with pixels the same by coping the first wone to the
+                    //next width-1 pixels
+                    out.at<cv::Vec3b>(i,j+k)[1] = img.at<cv::Vec3b>(i,j)[1];
+                    out.at<cv::Vec3b>(i,j+k)[2] = img.at<cv::Vec3b>(i,j)[2];
+                }
+            }
+        } else { //odd lines
+            if(odd_smpl_w == 0) {
+                for (int j=0; j<img.cols; j++) {
+                    //copy croma of pixel above
+                    out.at<cv::Vec3b>(i,j)[1] = img.at<cv::Vec3b>(i-1,j)[1];
+                    out.at<cv::Vec3b>(i,j)[2] = img.at<cv::Vec3b>(i-1,j)[2];
+                }
+            } else {
+                for (int j=0; j<img.cols; j+=odd_smpl_w) {
+                    for(int k = 1; k<odd_smpl_w; k++){
+                        //make smpl with pixels the same by coping the first wone to the
+                        //next width-1 pixels
+                        out.at<cv::Vec3b>(i,j+k)[1] = img.at<cv::Vec3b>(i,j)[1];
+                        out.at<cv::Vec3b>(i,j+k)[2] = img.at<cv::Vec3b>(i,j)[2];
+                    }
+                }
+            }
         }
     }
     return out;
--- assignment_2/CMPT365A2/math.h
+++ assignment_2/CMPT365A2/math.h
@@ -6,16 +6,22 @@
 #include <opencv2/imgcodecs.hpp>
 #include <opencv2/opencv.hpp>
 
+//########
+//UNTESTED
+//########
 //converts a 3 byte rgb pixel to yub
 cv::Vec3b rgb2yuv(cv::Vec3b pixel);
 
 //converts a cv image from rgb to yuv
-cv::Mat rgb2yuv(cv::Mat img);
+cv::Mat rgb2yuv(cv::Mat &img);
 
 //converta a yub pixel to rgb
 cv::Vec3b yuv2rgb(cv::Vec3b pixel);
 
 //converts a yuv cv img to rgb
-cv::Mat yuv2rgb(cv::Mat img);
+cv::Mat yuv2rgb(cv::Mat &img);
 
-
+cv::Mat chroma_subsample(cv::Mat &img, int a=4, int b=2, int c=0);
+//######
+//TESTED
+//######
